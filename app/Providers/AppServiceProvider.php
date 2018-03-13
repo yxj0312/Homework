ffa16305a6fbb1f8c240663a4acbf80d751b8ac2
yxj0312 <yanxiajun@hotmail.com>
2018-03-13 23:25:27 +0100

    EP13 View Composer

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,17 +3,29 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Channel;
 
 class AppServiceProvider extends ServiceProvider
 {
     /**
      * Bootstrap any application services.
-     *
+     * Here means, wenn a view is loaded, we always get a channel variable. 
+     * 
+     * Alternatively, you can create a new serviceprovider like ViewServiceProvider
+     * 
      * @return void
      */
     public function boot()
     {
-        //
+        // \View::composer(['threads.create','threads.index'], function ($view) {
+        
+        /* \View::composer('*', function($view){
+            $view->with('channels',\App\Channel::all());
+        }); */
+
+        //equal to above
+        \View::share('channels', Channel::all());
+
     }
 
     /**
