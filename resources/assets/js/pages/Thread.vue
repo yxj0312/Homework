59daebf1c6d62f31699336c92e234f2571418c9f
yxj0312 <yanxiajun@hotmail.com>
2018-05-03 22:38:22 +0200

    Ep 93 A Thread Can Be Updated: Part 3
    - Two issues are created, need to be solved later

--- resources/assets/js/pages/Thread.vue
+++ resources/assets/js/pages/Thread.vue
@@ -14,16 +14,57 @@
             return {
                 repliesCount: this.thread.replies_count,
                 locked: this.thread.locked,
-                editing: false
+                editing: false,
+                title: this.thread.title,
+                body: this.thread.body,
+                // use v-model to bind this.
+                form: {
+                    //  We can move it to created()
+                    // title: this.thread.title,
+                    // body: this.thread.body
+                }
             };
+        },
+
+        created() {
+            this.resetForm();
         },
 
         methods: {
             toggleLock() {
+                let uri = `/locked-threads/${this.thread.slug}`;
                 // ajax
-                axios[this.locked ? 'delete' : 'post']('/locked-threads/'+ this.thread.slug);
+                axios[this.locked ? 'delete' : 'post'](uri);
 
                 this.locked = ! this.locked;
+            },
+
+            /* cancel() {
+                // Another appoach.
+                // this.form = {
+                //     title: this.thread.title,
+                //     body: this.thread.body
+                // };
+            }, */
+
+            update() {
+                let uri = `/threads/${this.thread.channel.slug}/${this.thread.slug}`;
+
+                axios.patch(uri, this.form).then(()=>{
+                    this.editing = false;
+                    this.title = this.form.title;
+                    this.body = this.form.body;
+                    flash('Your thread has been updated.');
+                });
+            },
+
+            resetForm() {
+                this.form = {
+                    title: this.thread.title,
+                    body: this.thread.body,
+                };
+
+                this.editing = false;
             }
         }
     }
--- resources/views/threads/_question.blade.php
+++ resources/views/threads/_question.blade.php
@@ -2,21 +2,21 @@
 <div class="card card-default" v-if="editing">
     <div class="card-header">
         <div class="level">
-            <input type="text" value="{{ $thread->title }}" class="form-control">    
+            <input type="text" class="form-control" v-model="form.title">    
         </div>
     </div>
     
     <div class="card-body">
         <div class="form-group">
-            <textarea class="form-control" rows="10">{{ $thread->body }}</textarea>
+            <textarea class="form-control" rows="10" v-model="form.body"></textarea>
         </div>
     </div>
 
     <div class="card-footer">
         <div class="level">
             {{-- <button class="btn btn-xs level-item" @click="editing = true" v-show="! editing">Edit</button> --}}
-            <button class="btn btn-primary btn-xs level-item" @click="">Update</button>            
-            <button class="btn btn-xs level-item" @click="editing = false">Cancel</button>            
+            <button class="btn btn-primary btn-xs level-item" @click="update">Update</button>            
+            <button class="btn btn-xs level-item" @click="resetForm">Cancel</button>            
 
             @can ('update', $thread)
                 <form action="{{ $thread->path() }}" method="POST" class="ml-a">
@@ -35,18 +35,16 @@
         <div class="level">
             <img src="{{ $thread->creator->avatar_path }}" alt="{{ $thread->creator->name }}" width="25" height="25" class="mr-1">
             <span class="flex">
-                <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted:
-                {{ $thread->title }}
+                <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a> posted: <span v-text="title"></span>
             </span> 
             
         </div>
     </div>
 
-    <div class="card-body">
-        {{ $thread->body }}
+    <div class="card-body" v-text="body">
     </div>
 
-    <div class="card-footer">
+    <div class="card-footer" v-if="authorize('owns',thread)">
         <button class="btn btn-xs" @click="editing = true">Edit</button>
     </div>
 </div>
