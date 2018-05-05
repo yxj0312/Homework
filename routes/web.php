a81f3f044761d682cd824e403e87f5cbd19ae524
yxj0312 <yanxiajun@hotmail.com>
2018-05-05 13:49:26 +0200

    Ep 95 First Class Search: Implementation

--- routes/web.php
+++ routes/web.php
@@ -20,6 +20,8 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');  
 Route::get('threads', 'ThreadController@index')->name('threads');
 Route::get('threads/create', 'ThreadController@create')->name('threads.create');
+Route::get('threads/search', 'SearchController@show');
+
 Route::get('threads/{channel}/{thread}','ThreadController@show')->name('threads.show');
 Route::patch('threads/{channel}/{thread}','ThreadController@update')->name('threads.update');
 Route::delete('threads/{channel}/{thread}', 'ThreadController@destroy');
