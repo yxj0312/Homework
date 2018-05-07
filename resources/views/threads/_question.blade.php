460f21d58653c01e48a879fa0af38920c5d0ecc0
yxj0312 <yanxiajun@hotmail.com>
2018-05-07 22:31:10 +0200

    Ep 99 WYSIWYG

--- resources/views/threads/_question.blade.php
+++ resources/views/threads/_question.blade.php
@@ -8,7 +8,8 @@
     
     <div class="card-body">
         <div class="form-group">
-            <textarea class="form-control" rows="10" v-model="form.body"></textarea>
+            {{-- <textarea class="form-control" rows="10" v-model="form.body"></textarea> --}}
+            <wysiwyg v-model="form.body" :value="form.body"></wysiwyg>
         </div>
     </div>
 
@@ -41,7 +42,7 @@
         </div>
     </div>
 
-    <div class="card-body" v-text="body">
+    <div class="card-body" v-html="body">
     </div>
 
     <div class="card-footer" v-if="authorize('owns',thread)">
