22f1ee8d479d9deaa361401b713d7ed6c63114d5
yxj0312 <yanxiajun@hotmail.com>
2018-05-20 17:41:09 +0200

    A Ep 08 Feature: Reducing Reputation

--- tests/Feature/ReputationTest.php
+++ tests/Feature/ReputationTest.php
@@ -5,6 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use App\Reputation;
 
 class ReputationTest extends TestCase
 {
@@ -15,7 +16,21 @@ class ReputationTest extends TestCase
     {
         $thread = create('App\Thread');
 
-        $this->assertEquals(10, $thread->creator->reputation);
+        $this->assertEquals(Reputation::THREAD_WAS_PUBLISHED, $thread->creator->reputation);
+    }
+
+    /** @test */
+    function a_user_lose_points_when_they_delete_a_thread()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread', ['user_id' => auth()->id()]);
+
+        $this->assertEquals(Reputation::THREAD_WAS_PUBLISHED, $thread->creator->reputation);
+
+        $this->delete($thread->path());
+
+        $this->assertEquals(0, $thread->creator->fresh()->reputation);
     }
 
     /** @test */
@@ -28,7 +43,21 @@ class ReputationTest extends TestCase
             'body' => 'Here is a reply'
         ]);
 
-        $this->assertEquals(2, $reply->owner->reputation);
+        $this->assertEquals(Reputation::REPLY_POSTED, $reply->owner->reputation);
+    }
+
+    /** @test */
+    function a_user_loses_points_when_their_reply_to_a_thread_is_deleted()
+    {
+        $this->signIn();
+
+        $reply = create('App\Reply', ['user_id' => auth()->id()]);
+
+        $this->assertEquals(Reputation::REPLY_POSTED, $reply->owner->reputation);
+
+        $this->delete("/replies/{$reply->id}");
+
+        $this->assertEquals(0, $reply->owner->fresh()->reputation);
     }
 
     /** @test */
@@ -36,13 +65,56 @@ class ReputationTest extends TestCase
     {
         $thread = create('App\Thread');
 
-        $reply = $thread->addReply([
+        // $reply = $thread->addReply([
+        //     'user_id' => create('App\User')->id,
+        //     'body' => 'Here is a reply'
+        // ]);
+
+        $thread->markBestReply($reply = $thread->addReply([
             'user_id' => create('App\User')->id,
             'body' => 'Here is a reply'
+        ]));
+
+        $total = Reputation::REPLY_POSTED + Reputation::BEST_REPLY_AWARDED;
+        $this->assertEquals($total, $reply->owner->reputation);
+    }
+
+    /** @test */
+    function a_user_earns_points_when_their_reply_is_favorited()
+    {
+        $this->signIn();
+
+        $thread = create('App\Thread');
+
+        $reply = $thread->addReply([
+            'user_id' => auth()->id(),
+            'body' => 'Some reply'
         ]);
 
-        $thread->markBestReply($reply);
+        $this->post("/replies/{$reply->id}/favorites");
 
-        $this->assertEquals(52, $reply->owner->reputation);
+        $total = Reputation::REPLY_POSTED + Reputation::REPLY_FAVORITED;
+
+        $this->assertEquals($total, $reply->owner->fresh()->reputation);
+    }
+
+    /** @test */
+    function a_user_loses_points_when_their_favorited_reply_is_unfavorited()
+    {
+        $this->signIn();
+
+        $reply = create('App\Reply', ['user_id' => auth()->id()]);
+
+        $this->post("/replies/{$reply->id}/favorites");
+
+        $total = Reputation::REPLY_POSTED + Reputation::REPLY_FAVORITED;
+
+        $this->assertEquals($total, $reply->owner->fresh()->reputation);
+
+        $this->delete("/replies/{$reply->id}/favorites");
+
+        $total = Reputation::REPLY_POSTED + Reputation::REPLY_FAVORITED - Reputation::REPLY_FAVORITED;
+        
+        $this->assertEquals($total, $reply->owner->fresh()->reputation);        
     }
 }
