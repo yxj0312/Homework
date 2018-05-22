53c4408835442b435fb7fe82f3b677e9d113905f
yxj0312 <yanxiajun@hotmail.com>
2018-05-22 21:54:20 +0200

    wysiwyg-cleanup #20

--- resources/assets/js/components/Wysiwyg.vue
+++ resources/assets/js/components/Wysiwyg.vue
@@ -2,25 +2,48 @@
     <div>
         <input id="trix" type="hidden" :name="name" :value="value">
 
-        <trix-editor ref="trix" input="trix" :placeholder="placeholder"></trix-editor>
+        <trix-editor
+            ref="trix"
+            input="trix"
+            @trix-change="change"
+            :placeholder="placeholder">
+        </trix-editor>
     </div>
 </template>
+
+<style lang="scss">
+    @import '~trix/dist/trix.css';
+</style>
 
 <script>
     import Trix from 'trix';
 
     export default {
-        props: ['name', 'value', 'placeholder', 'shouldClear'],
+        props: ['name', 'value', 'placeholder'],
 
-        mounted() {
-            this.$refs.trix.addEventListener('trix-change', e => {
-                this.$emit('input', e.target.innerHTML);
-            });
+        methods: {
+            change({target}) {
+                this.$emit('input', target.value)
+            }
+        },
 
-            this.$watch('shouldClear', () => {
-                this.$refs.trix.value = '';
-                this.$emit('inputCleared');
-            })
+        watch: {
+            value(val) {
+                if (val === '') {
+                    this.$refs.trix.value = '';
+                }
+            }
         }
+        // mounted() {
+        //     this.$refs.trix.addEventListener('trix-change', e => {
+        //         this.$emit('input', e.target.innerHTML);
+        //     });
+
+
+        //     this.$watch('shouldClear', () => {
+        //         this.$refs.trix.value = '';
+        //         this.$emit('inputCleared');
+        //     })
+        // }
     }
 </script>
