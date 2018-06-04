e1591cea6031810450515142a33eb14d17e3a69f
yxj0312 <yanxiajun@hotmail.com>
2018-06-04 22:34:42 +0200

    thread-channel-label #21

--- app/Http/Controllers/ThreadController.php
+++ app/Http/Controllers/ThreadController.php
@@ -169,7 +169,9 @@ class ThreadController extends Controller
     protected function getThreads(Channel $channel, ThreadFilters $filters)
     {
         // $threads = Thread::latest()->filter($filters);
-        $threads = Thread::latest('pinned')->latest()->filter($filters);
+        // $threads = Thread::latest('pinned')->latest()->filter($filters);
+        $threads = Thread::latest('pinned')->latest()->with('channel')->filter($filters);
+
 
         //Queries reduction: eager load with 'with'
         // $threads = Thread::with('channel')->latest()->filter($filters);
