845321eff0aae39eea45971c4e4214f92072a2ca
yxj0312 <yanxiajun@hotmail.com>
2018-06-10 22:20:45 +0200

    Change subscribeButton.vue #35

--- resources/assets/js/components/SubscribeButton.vue
+++ resources/assets/js/components/SubscribeButton.vue
@@ -1,49 +1,59 @@
 <template>
     <!-- refactor, cause written in computed -->
     <!-- <button class="btn" :class="active ? 'btn-primary' : 'btn-default'" @click = "subscribe">Subscribe</button> -->
-    <button :class="classes" @click = "toggleSubscribe" v-text="toggle"></button>
+    <!-- <button :class="classes" @click = "toggleSubscribe" v-text="toggle"></button> -->
+    <a href="#" class="ml-2 pl-2 border-l" :class="isActive ? 'font-bold': ''" @click.prevent="subscribe" v-text="isActive ? 'Subscribed' : 'Subscribe'"></a>
 </template>
 
 <script>
-    export default {
-        props: ['active'],
+export default {
+  props: ['active'],
 
-        // components: {},
+  // components: {},
 
-        // Git rid of this session entirely, cause we set props above
-        data() {
-            return {
-                // Toogle this in Chrome console: $vm0.active = false;
-                mutableActive: this.active,
-            }
-        },
+  // Git rid of this session entirely, cause we set props above
+  data() {
+    return {
+      // Toogle this in Chrome console: $vm0.active = false;
+      isActive: this.active
+    };
+  },
 
-        computed: {
-            classes() {
-                return ['btn', this.mutableActive ? 'btn-primary' : 'btn-default'];
-            },
+  // computed: {
+  //     classes() {
+  //         return ['btn', this.isActive ? 'btn-primary' : 'btn-default'];
+  //     },
 
-            toggle() {
-                return this.mutableActive ? 'UnSubscribe' : 'Subscribe'
-            }
-        },
+  //     toggle() {
+  //         return this.isActive ? 'UnSubscribe' : 'Subscribe'
+  //     }
+  // },
 
-        methods: {
-            toggleSubscribe() {
-                /* let requestType = this.active ? 'delete' : 'post';
+  methods: {
+    //     toggleSubscribe() {
+    //         /* let requestType = this.active ? 'delete' : 'post';
 
-                axios[requestType](location.pathname + '/subscriptions'); */
+    //         axios[requestType](location.pathname + '/subscriptions'); */
 
-                axios[
-                    (this.mutableActive ? 'delete' : 'post')
-                    ](location.pathname + '/subscriptions');
-                
-                this.mutableActive = !this.mutableActive;
-                // this.mutableActive ? this.mutableActive = false : this.mutableActive = true
-                // this.active = !this.active;              
-            }
+    //         axios[
+    //             (this.isActive ? 'delete' : 'post')
+    //             ](location.pathname + '/subscriptions');
 
+    //         this.isActive = !this.isActive;
+    //         // this.mutableActive ? this.mutableActive = false : this.mutableActive = true
+    //         // this.active = !this.active;
+    //     }
 
-        }
+    // }
+    subscribe() {
+      axios[this.isActive ? 'delete' : 'post'](
+        location.pathname + '/subscriptions'
+      );
+      this.isActive = !this.isActive;
+      if (this.isActive) {
+        flash("Okay, we'll notify you when this thread is updated!");
+      }
     }
+  }
+};
 </script>
