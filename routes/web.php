254dc8a633ab45ca6f8d4d089228af918894b67c
XiajunYan <xiajun.yan@dekra.com>
2018-06-11 17:19:44 +0200

    Remove welcome page and related route

--- routes/web.php
+++ routes/web.php
@@ -11,15 +11,11 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::redirect('/', 'threads');
 
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-
-Route::view('scan', 'scan');
 
 Route::get('threads', 'ThreadController@index')->name('threads');
 Route::get('threads/create', 'ThreadController@create')->name('threads.create')->middleware('must-be-confirmed');
