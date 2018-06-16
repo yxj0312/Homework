1b5998dbe77e3fed44fc8ca8db69d683956e3cd9
yxj0312 <yanxiajun@hotmail.com>
2018-06-16 23:39:46 +0200

    Refactor mention event and fix the test - resolves #39

--- app/Events/ThreadReceiveNewReply.php
+++ app/Events/ThreadReceiveNewReply.php
@@ -3,20 +3,31 @@
 namespace App\Events;
 
 use Illuminate\Queue\SerializesModels;
+use Illuminate\Foundation\Events\Dispatchable;
 
 class ThreadReceiveNewReply
 {
-    use SerializesModels;
-
+    use Dispatchable, SerializesModels;
+    /**
+     * The reply that was posted.
+     *
+     * @param \App\Reply $reply
+     */
     public $reply;
-
     /**
      * Create a new event instance.
      *
-     * @return void
+     * @param \App\Reply $reply
      */
     public function __construct($reply)
     {
         $this->reply = $reply;
+    }
+    /**
+     * Get the subject of the event.
+     */
+    public function subject()
+    {
+        return $this->reply;
     }
 }
--- tests/Feature/MentionUsersTest.php
+++ tests/Feature/MentionUsersTest.php
@@ -2,13 +2,53 @@
 
 namespace Tests\Feature;
 
+use App\Thread;
+use App\Mentions;
 use Tests\TestCase;
+use App\Rules\Recaptcha;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class MentionUsersTest extends TestCase
 {
     use RefreshDatabase;
+
+    public function setUp()
+    {
+        parent::setUp();
+
+        app()->singleton(Recaptcha::class, function () {
+            return \Mockery::mock(Recaptcha::class, function ($m) {
+
+			// If it recevie it should call the 'passes' method, it should return true.
+                $m->shouldReceive('passes')->andReturn(true);
+            });
+        });
+    }
+
+    /** @test */
+    public function mentioned_users_in_a_thread_are_notified()
+    {
+        // Given we have a user, JohnDoe, who is signed in.
+        $john = create(\App\User::class, ['name' => 'JohnDoe']);
+        $this->signIn($john);
+        // And we also have a user, JaneDoe.
+        $jane = create(\App\User::class, ['name' => 'JaneDoe']);
+        // And JohnDoe create a new thread and mentions @JaneDoe.
+        $thread = make(\App\Thread::class, [
+            'body' => 'Hey @JaneDoe check this out.'
+        ]);
+
+        $this->post(route('threads'), $thread->toArray() + ['g-recaptcha-response' => 'token']);
+        
+        // Then @JaneDoe should receive a notification.
+        $this->assertCount(1, $jane->notifications);
+        $this->assertEquals(
+            "JohnDoe mentioned you in \"{$thread->title}\"",
+            $jane->notifications->first()->data['message']
+        );
+    }
 
     /** @test */
     public function mentioned_users_in_a_reply_are_notified()
