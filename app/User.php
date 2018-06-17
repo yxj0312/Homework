ebdb97d106ee69a9025b6a6de0b3056cd17f5dcf
yxj0312 <yanxiajun@hotmail.com>
2018-06-17 23:30:20 +0200

    Case insensitive comparison against config admins #21

--- app/User.php
+++ app/User.php
@@ -86,7 +86,11 @@ class User extends Authenticatable
 
     public function isAdmin()
     {
-        return in_array($this->email, config('homework.administrators'));
+        // return in_array($this->email, config('homework.administrators'));
+        return in_array(
+            strtolower($this->email),
+            array_map('strtolower', config('homework.administrators'))
+        );
     }
 
     /**
