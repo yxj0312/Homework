ad951839d9d91a9f4ce38b4e79e4d75bf4cb074c
yxj0312 <yanxiajun@hotmail.com>
2018-06-17 23:42:29 +0200

    Save content of the body input if validation fails #21

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -13,24 +13,33 @@
                     <div class="card-body">
                       <form method="POST" action="{{ route('threads.store') }}">
                         {{ csrf_field() }}
+
                             <div class="form-group">
                                 <label for="channel_id">Choose a Channel:</label>
                                 <select name="channel_id" id="channel_id" class="form-control" required>
                                     <option value="" disabled selected="selected">Choose one...</option>
+
                                     @foreach ($channels as $channel)
-                                        <option value="{{ $channel->id}}" {{ old('channel_id')== $channel->id ? 'selected' : ''}}>{{ $channel->name }}</option>
+                                        <option value="{{ $channel->id}}" {{ old('channel_id')== $channel->id ? 'selected' : ''}}>
+                                            {{ $channel->name }}
+                                        </option>
                                     @endforeach
                                 </select>
                             </div>
 
                             <div class="form-group">
                                 <label for="title">Title:</label>
-                                <input type="text" name="title" id="title" class="form-control" value="{{ old('title') }}" placeholder="title" required>
+                                <input type="text"
+                                    name="title"
+                                    id="title"
+                                    class="form-control"
+                                    value="{{ old('title') }}"
+                                    placeholder="title" required>
                             </div>
                             <div class="form-group">
                                 <label for="body">Body:</label>
                                 {{-- <textarea name="body" id="body" class="form-control" rows="8" required>{{ old('body') }}</textarea> --}}
-                                <wysiwyg name="body"></wysiwyg>
+                                <wysiwyg name="body" value="{{ old('body') }}"></wysiwyg>
                             </div>
                             <div class="form-group">
                                 <div class="g-recaptcha" data-sitekey="{{ config('homework.recaptcha.key') }}"></div>
